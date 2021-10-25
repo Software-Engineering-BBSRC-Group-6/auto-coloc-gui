72ab2a111aab6379797d6fd740f3886d66512ad6
Cameron Anderson <cagfa1@users.noreply.github.com>
2021-10-25 17:41:57 +0100

    continuation

--- coloc/gui/giu_main.py
+++ coloc/gui/giu_main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import pyqtSlot
+import os
 
 class App(QMainWindow):
 
@@ -41,35 +42,35 @@ class MyTableWidget(QWidget):
         self.tab1.layout = QGridLayout(self)
 
         self.openFile1 = QPushButton("Open File")
-        self.openFile1Label = QLabel('Select Input File:')
+        self.openFile1Label = QLabel('Select Input Images (.tiff):')
+        self.fileLabel1 = QLabel()
+        self.fileLabel1.setMaximumWidth(100)
         self.thresholdInput = QLineEdit()
         self.thresholdInput.setPlaceholderText("0.1 <= Threshold <= 0.9")
         self.thresholdLabel = QLabel('Input Threshold Value:') #Add a check that this is a float
         self.dropdown1 = QComboBox()
         self.dropdownLabel = QLabel('Select Colocalisation Statistic:')
-        self.checkBox1 = QCheckBox()
-        self.checkboxLabel = QLabel('Save Output?')
-        self.openFile2 = QPushButton("Open Folder")
-        self.openFile2Label = QLabel('Select Output Folder:')
         self.runButton = QPushButton("Run")
-        self.cancelButton = QPushButton("Cancel")
+        self.cancelButton = QPushButton("Stop")
         
         self.tab1.layout.addWidget(self.openFile1, 1, 3)
         self.tab1.layout.addWidget(self.openFile1Label, 1, 2)
+        self.tab1.layout.addWidget(self.fileLabel1, 1, 4)
         self.tab1.layout.addWidget(self.thresholdInput, 2, 3)
         self.tab1.layout.addWidget(self.thresholdLabel, 2, 2)
         self.tab1.layout.addWidget(self.dropdown1, 3, 3)
         self.tab1.layout.addWidget(self.dropdownLabel, 3, 2)
-        self.tab1.layout.addWidget(self.checkBox1, 4, 3)
-        self.tab1.layout.addWidget(self.checkboxLabel, 4,2)
-        self.tab1.layout.addWidget(self.openFile2, 5, 3)
-        self.tab1.layout.addWidget(self.openFile2Label, 5, 2)
-        self.tab1.layout.addWidget(self.runButton, 6, 5)
-        self.tab1.layout.addWidget(self.cancelButton, 6, 4)
+        self.tab1.layout.addWidget(self.runButton, 6, 6)
+        self.tab1.layout.addWidget(self.cancelButton, 6, 5)
         self.tab1.setLayout(self.tab1.layout)
 
+        self.dropdown1.addItems(["--Statistical Method--", "Pearson ", "Spearman", "Wilcoxon"])
+
+
+        self.runButton.setDisabled(True)
+        self.cancelButton.setDisabled(True)
+
         self.openFile1.clicked.connect(self.define_file_path)
-        self.openFile2.clicked.connect(self.define_directory_path)
         
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
@@ -83,10 +84,19 @@ class MyTableWidget(QWidget):
     
     def define_file_path(self):
         self.in_path = QFileDialog.getOpenFileName(self,"Choose TIF File To Open","E:\\")[0]
+        self.fileLabel1.setText('{}'.format(self.in_path))
+        self.out_path = os.path.basename(self.in_path)
     
     def define_directory_path(self):
         self.out_path = QFileDialog.getExistingDirectory(self,"Choose Directory To Save","E:\\")
     
+    def activate_save_output(self):
+        if self.checkBox1.isChecked():
+            self.openFile2.setDisabled(False)
+            self.openFile2Label.setDisabled(False)
+        else:
+            self.openFile2.setDisabled(True)
+            self.openFile2Label.setDisabled(True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
