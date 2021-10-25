308b129b4ed5ea3a96cc9ff383739f5cef015c3f
FusionLocus <joejpollacco@gmail.com>
2021-10-25 16:58:21 +0100

    Updated class definitions, created pipeline for class use

--- coloc/backend/classes.py
+++ coloc/backend/classes.py
@@ -1,91 +1,99 @@
 from datetime import datetime
 import os
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class pipeline_object():
     """Pipeline object class. Any image becomes an instance of this class.
     The class operates using its methods to move an image through the pipeline.
     """
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self, inpath, outpath, threshold=False):
+        self.filepath = inpath
+        self.outpath = outpath
         self.timestamp = (datetime.now()).strftime('%Y%m%d-%H%M%S')
-        self.temp_dir_path = os.path.join('./data/output/', 
-                                          self.timestamp + '/')
-        if not os.path.exists(self.temp_dir_paths):
-            os.mkdir(self.temp_dir_path)
+        self.threshold = threshold
 
-    def reshape_im(self):
-        """Reshapes an image such that the dimensions are square, using the dimension
-        of the smallest side.
-
-        :param sourcefile: input multi-image .tiff
-        :type sourcefile: string
-
-        :return resized_file_path: Path to the new, downsized file.
-        :type resized_file_path: string
+    def split(self):
+        """Split input .tiff file into separate RGB slices
         """
         im = Image.open(self.filepath)
-        # Find smallest dimension n and set image size to square n x n.
-        smallest_dim = min(im.size)
-        im.resize([smallest_dim, smallest_dim], resample=Image.LANCZOS)
-        resized_file_path = os.path.join(self.filepath, 'resized',
-                                         os.path.basename(self.filepath))
-        # Save and keep the new path as a class variable.
-        im.save(resized_file_path)
-        self.resized_file_path = resized_file_path
-
-        return
-    
-    def split(self):
-        """Split input .tiff file into separate RGB files and save to a sub-directory
-
-        :param sourcefile: input multi-image .tiff
-        :type sourcefile: string
-
-        :return: list of file paths for image import'''
-        """
-        im = Image.open(self.resized_file_path)
-        self.num_frames = im.n_frames
-
+        self.image_obj = im
+        self.num_frames = self.image_obj.n_frames
+        self.smallest_dim = min(self.image_obj.size)
+        self.frames = np.empty((self.num_frames, 3,
+                                self.smallest_dim, self.smallest_dim,))
         for i in range(self.num_frames):
-            im.save(os.path.join(self.temp_dir_path, 'page_{0}.tif'.format(i)))
-        
+            self.frames[i, :, :, :] = np.moveaxis(np.asarray(self.reshape_frame(i)),-1,0)
+
         return
 
-    def rescale(im_2D, threshold=False):
-        """Minmax rescale a 2D image
+    def reshape_frame(self, i):
+        """Reshapes an image such that the dimensions are square, using the dimension
+        of the smallest side.
+        """
+        self.image_obj.seek(i)
+        # Find smallest dimension n and set image size to square n x n.
+        resized = self.image_obj.resize((self.smallest_dim, self.smallest_dim),
+                                        resample=Image.LANCZOS)
 
-        :param im_2D: input array
-        :type im_2D: numpy array
+        # Keep the new image array as a class variable.
 
-        return: rescaled image"""
+        return resized
+
+    def rescale(self, j, i):
+        """Minmax rescale a 2D image at indices (i, j), where
+        j is the channel index and i the frame index.
+
+        :param j: index of channel
+        :type j: integer
+        :param i: index of frame
+        :type i: integer"""
+        im_2D = self.frames[i, j, :, :]
 
         if len(np.shape(im_2D)) != 2:
             raise ValueError("Input image should have two dimensions")
         if im_2D.all() == 0:
-            return im_2D
-        elif threshold:
-            im_2D = (im_2D-im_2D.min())/(im_2D.max()-im_2D.min())
-            trim = im_2D < threshold
-            im_2D[trim] = 0
-            return im_2D
+            return False
+        elif self.threshold:
+            self.frames[i, j, :, :] = (im_2D-im_2D.min())/(im_2D.max()-im_2D.min())
+            trim = self.frames[i, j, :, :] < self.threshold
+            self.frames[i, j, trim] = 0
+            return True
         else:
-            return (im_2D-im_2D.min())/(im_2D.max()-im_2D.min())
+            self.frames[i, j, :, :] = (im_2D-im_2D.min())/(im_2D.max()-im_2D.min())
+            return True
     
-    def rescale_stack(im_3d, threshold=False):
-    """Rescale RGB image using minmax rescaling
+    def rescale_all(self):
+        """Rescale RGB image using minmax rescaling
 
-    :param im_3d: input RGB image
-    :type im_3d: numpy array"""
+        :param im_3d: input RGB image
+        :type im_3d: numpy array"""
 
-    stack = ['R', 'G', 'B']
-    out = []
-    s = 0
-    for channel in [im_3d[:, :, i] for i in range(im_3d.shape[-1])]:
-        out.append(rescale(channel, threshold))
-        s += 1
-    return np.dstack(out)
+        for i in range(self.frames.shape[0]):
+            for j in range(self.frames.shape[1]):
+                self.rescale(j, i)
+
+        return True
+
+    def visualise(self):
+        """Visualise the stack of RGB images."""
+        im = Image.open(self.filepath)
+        for i in range(self.frames.shape[-1]):
+            im.seek(i)
+            plt.imshow(np.asarray(im))
+            plt.title("Original image {0}".format(str(i+1)))
+            plt.colorbar
+            plt.show()
+            plt.imshow(np.moveaxis(self.frames[i, :, :, :],0,-1))
+            plt.title("Processed image {0}".format(str(i+1)))
+            plt.colorbar
+            plt.show()
+        
+
+
+    
         
         
         
