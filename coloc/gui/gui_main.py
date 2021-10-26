2715b38cdf3fe85fd00138a4d2d39f64efa260b2
Cameron Anderson <cagfa1@users.noreply.github.com>
2021-10-26 16:44:27 +0100

    modifying to meet dictionary needs for Dan, including error message for check boxes, adding docstrings

--- coloc/gui/gui_main.py
+++ coloc/gui/gui_main.py
@@ -42,9 +42,12 @@ class MyTableWidget(QWidget):
         # Create first tab
         self.tab1.layout = QGridLayout(self)
 
+        # Define logo label
         self.logo = QLabel()
         pixmap = QPixmap('acg_logo.png')
         self.logo.setPixmap(pixmap)
+
+        # Set instruction labels
         self.introLabel1 = QLabel('Welcome to Auto-Colloc-GUI!')
         self.introLabel1.setFont(QFont('Ariel',weight=QFont.Bold))
         self.introLabel2 = QLabel('\nInput .tif multiple channel fluorescent image data to run the automated colocalisation analysis:')
@@ -52,19 +55,33 @@ class MyTableWidget(QWidget):
         self.introLabel3 = QLabel('\nDefine colocalisation parameters including clustering threshold and statistical analysis type:')
         self.introLabel3.setFont(QFont('Ariel',italic=True))
 
-        self.in_path = 'Empty'
-
+        # Create widgets for the setup tab inlcuding: labels, buttons, dropdowns, checkboxes, int input windows.
         self.openFile1 = QPushButton("Open File")
         self.openFile1Label = QLabel('Select Input Images (.tif):')
         self.fileLabel1 = QLabel()
         self.fileLabel1.setMaximumWidth(300)
         self.thresholdDropdown = QComboBox()
         self.thresholdLabel = QLabel('Select Threshold Value:')
-        self.statsDropdown = QComboBox()
-        self.statsDropdownLabel = QLabel('Select Colocalisation Statistic:')
+        self.thresholdDropdown.addItems(["--Threshold Value--", "0.1 ", "0.2", "0.3", "0.4", "0.5 ", "0.6", "0.7", "0.8", "0.9"])
+        self.channelsDropdown = QComboBox()
+        self.channelsDropdownLabel = QLabel('Select Number of Channels:')
+        self.channelsDropdown.addItems(["--Channel Number--", "1 ", "2", "3"])
+        self.inputClusterNo = QPushButton("Input Value")
+        self.inputClusterNoLabel = QLabel('Input Cluster Number:')
+        self.clusterNoLabel = QLabel()
+        self.clusterNoLabel.setMaximumWidth(300)
+        self.inputMinDist = QPushButton("Input Value") # Could be replaced by a scale metric...
+        self.inputMinDistLabel = QLabel('Input Minimum Distance:')
+        self.minDistLabel = QLabel()
+        self.minDistLabel.setMaximumWidth(300)
+        self.intensitycorrCheckbox = QCheckBox()
+        self.intensitycorrLabel = QLabel('Run Intensity Correlation Analysis:')
+        self.kmeansCheckbox = QCheckBox()
+        self.kmeansLabel = QLabel('Run KMeans Analysis:')
         self.runButton = QPushButton("Run")
         self.cancelButton = QPushButton("Stop")
 
+        # Add widgets and define positioning on QGridLayout
         self.tab1.layout.addWidget(self.logo, 1, 3)
         self.tab1.layout.addWidget(self.introLabel1, 2, 3, 1, 2)
         self.tab1.layout.addWidget(self.introLabel2, 3, 2, 1, 2)
@@ -74,19 +91,35 @@ class MyTableWidget(QWidget):
         self.tab1.layout.addWidget(self.introLabel3, 5, 2, 1, 2)
         self.tab1.layout.addWidget(self.thresholdDropdown, 6, 3)
         self.tab1.layout.addWidget(self.thresholdLabel, 6, 2)
-        self.tab1.layout.addWidget(self.statsDropdown, 7, 3)
-        self.tab1.layout.addWidget(self.statsDropdownLabel, 7, 2)
-        self.tab1.layout.addWidget(self.runButton, 8, 6)
-        self.tab1.layout.addWidget(self.cancelButton, 8, 5)
+        self.tab1.layout.addWidget(self.channelsDropdown, 7, 3)
+        self.tab1.layout.addWidget(self.channelsDropdownLabel, 7, 2)
+        self.tab1.layout.addWidget(self.inputClusterNo, 8, 3)
+        self.tab1.layout.addWidget(self.inputClusterNoLabel, 8, 2)
+        self.tab1.layout.addWidget(self.clusterNoLabel, 8, 4, 1, 4)
+        self.tab1.layout.addWidget(self.inputMinDist, 9, 3)
+        self.tab1.layout.addWidget(self.inputMinDistLabel, 9, 2)
+        self.tab1.layout.addWidget(self.minDistLabel, 9, 4, 1, 4)
+        self.tab1.layout.addWidget(self.intensitycorrCheckbox, 10, 3)
+        self.tab1.layout.addWidget(self.intensitycorrLabel, 10, 2)
+        self.tab1.layout.addWidget(self.kmeansCheckbox, 11, 3)
+        self.tab1.layout.addWidget(self.kmeansLabel, 11, 2)
+        self.tab1.layout.addWidget(self.runButton, 12, 6)
+        self.tab1.layout.addWidget(self.cancelButton, 12, 5)
         self.tab1.setLayout(self.tab1.layout)
 
-        self.thresholdDropdown.addItems(["--Threshold Value--", "0.1 ", "0.2", "0.3", "0.4", "0.5 ", "0.6", "0.7", "0.8", "0.9"])
-        self.statsDropdown.addItems(["--Statistical Method--", "Pearson ", "Spearman", "Wilcoxon"])
-
+        # Disable/'grey-out' widgets
         self.cancelButton.setDisabled(True)
         self.fileLabel1.setDisabled(True)
+        self.clusterNoLabel.setDisabled(True)
+        self.minDistLabel.setDisabled(True)
             
+        # Define path as empty for later use
+        self.in_path = 'Empty'
+
+        # Connect GUI buttons to respective functions
         self.openFile1.clicked.connect(self.define_file_path)
+        self.inputClusterNo.clicked.connect(self.define_cluster_number)
+        self.inputMinDist.clicked.connect(self.define_min_distance)
         self.runButton.clicked.connect(self.run_program)
 
         # Add tabs to widget
@@ -99,43 +132,74 @@ class MyTableWidget(QWidget):
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
             print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
     
+    def define_file_path(self):
+        '''
+        This function uses the file path that the user selects as input to define in_path and out_path variables
+        as well as labelling the GUI with the selected file name to confirm file selection.
+        '''
+        self.in_path = QFileDialog.getOpenFileName(self,"Choose TIF File To Open","E:\\")[0]
+        self.out_path = self.in_path.replace(os.path.basename(self.in_path), "")
+        self.fileLabel1.setText('{}'.format(os.path.basename(self.in_path)))
+    
+    def define_cluster_number(self):
+        '''
+        This cluster labels the GUI with the user's cluster number input to confirm data entry.
+        '''
+        self.clusterInput, bool_info = QInputDialog.getInt(self, 'Integer Input Dialog', 'Enter cluster number:')
+        self.clusterNoLabel.setText(str(self.clusterInput))
+
+    def define_min_distance(self):
+        '''
+        This cluster labels the GUI with the user's minimum distance input to confirm data entry.
+        '''
+        self.distInput, bool_info = QInputDialog.getInt(self, 'Integer Input Dialog', 'Enter minimum distance:')
+        self.minDistLabel.setText(str(self.distInput))
+
     def start_animation(self):
+        '''
+        This function loads the loading.gif file as a movie attached to a label widget. The function adds the 
+        widget and then starts the .gif file to create movement.
+        '''
         self.loading = QLabel()
         self.gif = QMovie('loading.gif')
         self.loading.setMovie(self.gif)
-        self.tab1.layout.addWidget(self.loading, 8,7)
+        self.tab1.layout.addWidget(self.loading, 12, 7)
         self.gif.start()
 
     def stop_animation(self):
+        '''
+        This function stops the loading.gif movie and deletes the loading gif label so that the loading 
+        sign disappears.
+        '''
         self.gif.stop()
         self.tab1.layout.removeWidget(self.loading)
         self.loading = None
 
-    def define_file_path(self):
-        self.in_path = QFileDialog.getOpenFileName(self,"Choose TIF File To Open","E:\\")[0]
-        self.out_path = self.in_path.replace(os.path.basename(self.in_path), "")
-        # Changed in_path to out_path
-        self.fileLabel1.setText('{}'.format(os.path.basename(self.in_path)))
-        if self.in_path is not 'Empty' and os.path.splitext(self.in_path) not in ['.tif']:
-            self.correct_input = True
-        print(self.in_path)
-        print(self.out_path)
-
     def create_dict(self):
+        '''
+        This file creates a dictionary of the user inputs which can be fed to the model and visualiser functions.
+        '''
         dict_data = {}
-        dict_data["in_path"] = self.in_path
-        dict_data["out_path"] = self.out_path
-        dict_data["threshold"] = float(self.thresholdDropdown.currentText())
-        dict_data["statistic"] = str(self.statsDropdown.currentText())
+        # dict_data["in_path"] = self.in_path
+        # dict_data["out_path"] = self.out_path
+        # dict_data["threshold"] = float(self.thresholdDropdown.currentText())
+        # dict_data["statistic"] = str(self.statsDropdown.currentText())
         self.dict_data = dict_data
 
-    
     def check_errors(self):
+        '''
+        This function ensures that the user inputs are all complete. It checks that an input .tif file has been selected. It
+        also checks that necessary parameters have been selected for the model to run.
+        '''
+        # Access threshold and chennels inputs
         threshold_value = str(self.thresholdDropdown.currentText())
-        statistical_value = str(self.statsDropdown.currentText())
+        channels_value = str(self.channelsDropdown.currentText())
 
+        # Create Error message widgets
         inputMsg = QMessageBox()
         parameterMsg = QMessageBox()
+        analysisMsg = QMessageBox()
+
         inputMsg.setIcon(QMessageBox.Critical)
         inputMsg.setText("Input Error")
         inputMsg.setInformativeText('Please ensure a valid .tif file input is selected')
@@ -146,21 +210,38 @@ class MyTableWidget(QWidget):
         parameterMsg.setInformativeText('Please ensure all parameters have been selected')
         parameterMsg.setWindowTitle("Input Error")
 
+        analysisMsg.setIcon(QMessageBox.Critical)
+        analysisMsg.setText("Input Error")
+        analysisMsg.setInformativeText('Please ensure at least one statistical analysis has been checked/selected')
+        analysisMsg.setWindowTitle("Input Error")
+
+        # Set conditions for error messages to be delivered. Set booleans for check_error function to read. 
         if self.in_path is 'Empty' or os.path.splitext(self.in_path)[1] not in ['.tif']:
             inputMsg.exec_()
             return False
-        elif threshold_value in ['--Threshold Value--'] or statistical_value in ['--Statistical Method--']:
+        elif threshold_value in ['--Threshold Value--'] or channels_value in ['--Channel Number--'] or len(self.minDistLabel.text()) == 0 or len(self.minDistLabel.text()) == 0:
             parameterMsg.exec_()
+            return False
+        elif self.kmeansCheckbox.isChecked() == False and self.intensitycorrCheckbox.isChecked() == False:
+            analysisMsg.exec_()
             return False
         else:
             return True
 
     def cancel_clicked(self):
+        ''' 
+        This function is activated when the cancel button is clicked. It switches the activation of the run/cancel buttons and terminates
+        the loading .gif animation.
+        '''
         self.runButton.setDisabled(False)
         self.cancelButton.setDisabled(True)
         self.stop_animation()
 
     def run_program(self):
+        '''
+        This function is the overall controller of the script. It checks for input errors by calling the check_errors function. 
+        If this passes, the function creates the input dictionary, calls the model and the visualiser to display the results.
+        '''
         if self.check_errors() is False:
             return
         else:
